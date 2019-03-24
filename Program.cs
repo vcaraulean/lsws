89fcf25a7f89effe30f5b8b27f4186117aa1c1a0
Valeriu Caraulean <valeriu@caraulean.com>
2019-03-25 00:44:23 +0100

    Initial version

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace lsws
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebHost
+                .CreateDefaultBuilder(args)
+                .Configure(config => config.UseStaticFiles())
+                .UseWebRoot(Directory.GetCurrentDirectory()).Build()
+                .Run();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Local & Static Web Server
+
+Run in any folder to serve files as static content.
+
+## Prerequisites
+
+ - [.NET Core SDK 2.2] (https://dotnet.microsoft.com/download/dotnet-core/2.2)
+ 
+## Packaging
+
+```
+    dotnet tool install -g dotnet-warp
+    dotnet-warp
+```
